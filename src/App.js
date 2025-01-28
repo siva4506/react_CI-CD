076442e11d9f74a3aeef6feabe6f98a07c67845c
siva4506 <107547401+siva4506@users.noreply.github.com>
2025-01-29 01:33:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Jenkins Applications</h1>
+          <h1 className="App-title">Jenkins Don Applications</h1>
         </header>
           <Switch>
                 <Route exact path= "/" render={() => (
